c6d757d1386929154c89de549ea38644f95077c1
elpatron <aidanseantucker@gmail.com>
2023-10-11 15:05:59 -0400

    only one test isn't passing, but the code works as required?

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,34 @@ import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
 import itemData from "../data/items";
+import ItemForm from "./ItemForm";
 
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [updatedItems, setUpdatedItems] = useState([]);
+
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  const handleItemFormSubmit = (newItem) => {
+    setItems([...items, newItem]);
+  };
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
+      <ItemForm onItemFormSubmit={handleItemFormSubmit} />
       <ShoppingList items={items} />
+      <ul>
+        {updatedItems.map((item) => (
+          <li key={item.id}>
+            Name: {item.name}, Category: {item.category}
+          </li>
+        ))}
+      </ul>
 
     </div>
   );
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -4,48 +4,50 @@ import itemData from "../data/items";
 
 
 function ItemForm(props) {
-  // Create state variables for the form inputs
   const [name, setName] = useState("");
-  const [category, setCategory] = useState("Produce"); // Default to "Produce"
+  const [category, setCategory] = useState("Produce"); 
 
-  // Function to handle form submission
+  /////////////////////////////////////////////////////////////////
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("form submit successful")
     console.log(itemData)
+    
 
-    // Create a new food item object
     const newItem = {
       id: uuid(),
       name,
       category,
     };
 
-    // Call the function passed from the parent component to add the new item
+    props.onItemFormSubmit(newItem);
 
-
-    // Reset the form inputs
     setName("");
     setCategory("Produce");
   };
 
-  // Function to handle changes in the name input field
+  /////////////////////////////////////////////////////////////////
+
+
   const handleNameChange = (e) => {
     const newName = e.target.value;
     setName(newName);
 
-    // Log the user input for the name field to the console
     console.log("User input in Name field:", newName);
   };
 
-  // Function to handle changes in the category select field
+  /////////////////////////////////////////////////////////////////
+
   const handleCategoryChange = (e) => {
     const newCategory = e.target.value;
     setCategory(newCategory);
 
-    // Log the user input for the category field to the console
     console.log("User input in Category field:", newCategory);
   };
+
+
+
+  /////////////////////////////////////////////////////////////////
 
   return (
     <form className="NewItem" onSubmit={handleSubmit}>
@@ -55,7 +57,7 @@ function ItemForm(props) {
           type="text"
           name="name"
           value={name}
-          onChange={handleNameChange} // Call handleNameChange on input change
+          onChange={handleNameChange}
         />
       </label>
 
@@ -64,7 +66,7 @@ function ItemForm(props) {
         <select
           name="category"
           value={category}
-          onChange={handleCategoryChange} // Call handleCategoryChange on select change
+          onChange={handleCategoryChange}
         >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -74,7 +76,11 @@ function ItemForm(props) {
 
       <button type="submit">Add to List</button>
     </form>
+  
   );
 }
+
+
+
 
 export default ItemForm;
